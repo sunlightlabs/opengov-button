6eefd22875f2dba1c018a40a644593e25a617768
Albert Bowden <albert@sunlightfoundation.com>
2016-04-22 01:50:40 -0400

    browser, url detection

--- bookmarklet/opengov-button-inject.js
+++ bookmarklet/opengov-button-inject.js
@@ -15,6 +15,19 @@ function runScript() {
         		jq.src = 'jquery-1.8.3.min.js';
         		document.getElementsByTagName('head')[0].appendChild(jq);
         	}
+            function getBrowserId () {
+
+    var
+        aKeys = ["MSIE", "Firefox", "Safari", "Chrome", "Opera"],
+        sUsrAg = navigator.userAgent, nIdx = aKeys.length - 1;
+
+    for (nIdx; nIdx > -1 && sUsrAg.indexOf(aKeys[nIdx]) === -1; nIdx--);
+
+    return nIdx
+
+}
+
+console.log(getBrowserId());
 
 
 
