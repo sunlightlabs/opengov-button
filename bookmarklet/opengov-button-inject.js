43f7fec6af530d741c03641828c71e01bcc56f45
jalbertbowden <jalbertbowden@gmail.com>
2016-04-22 14:14:22 -0400

    content structure; min styles; user browser/pathname/url functionality

--- bookmarklet/opengov-button-inject.js
+++ bookmarklet/opengov-button-inject.js
@@ -15,29 +15,44 @@ function runScript() {
         		jq.src = 'jquery-1.8.3.min.js';
         		document.getElementsByTagName('head')[0].appendChild(jq);
         	}
-            function getBrowserId () {
 
-    var
-        aKeys = ["MSIE", "Firefox", "Safari", "Chrome", "Opera"],
-        sUsrAg = navigator.userAgent, nIdx = aKeys.length - 1;
-
-    for (nIdx; nIdx > -1 && sUsrAg.indexOf(aKeys[nIdx]) === -1; nIdx--);
-
-    return nIdx
-
-}
-
-console.log(getBrowserId());
+            var userDataList = "<ul class=\"xoxo\">\
+                <li>Browser Code: <b id=\"user-data-browser\"></b></li>\
+                <li>Browser: <b id=\"user-browser\"></b></li>\
+                <li>Pathname: <b id=\"user-pathname\"></b></li>\
+                <li>URL: <b id=\"user-url\"></b></li>\
+                </ul>";
 
 
+            // var htmlSTring = "<div>\
+            //  This is a string.\
+            //  </div>";
 
-        	var htmlSTring = "<div>\
-        		This is a string.\
-        		</div>";
-            var iframe = "<iframe src='http://sunlightfoundation.com/'></iframe>";
+            // inject opengov button content
+            $("body").append("<div id='opengov-button'><button id='closeopengovbutton' type='button'></button><div id='opengov-button-content'>" + userDataList + "</div></div>");
 
-        	// inject opengov button content
-        	$("body").append("<div id='opengov-button'><button id='closeopengovbutton' type='button'>" + iframe + "</div>");
+            var browserID, browserType = getBrowserId();
+
+            $('#user-data-browser').append(browserID);
+            $('#user-browser').append(browserType);
+            $('#user-browser').addClass(browserType);
+
+
+            // get url
+            var pathname = window.location.pathname; // Returns path only
+            var url      = window.location.href;     // Returns full URL
+            $("#user-pathname").append(pathname);
+            $("#user-url").append(url);
+
+            // get user browser
+            function getBrowserId (nIdx, aKey) {
+              var aKeys = ["MSIE", "Firefox", "Safari", "Chrome", "Opera"],
+                  sUsrAg = navigator.userAgent, nIdx = aKeys.length - 1;
+  
+                for (nIdx; nIdx > -1 && sUsrAg.indexOf(aKeys[nIdx]) === -1; nIdx--);
+                  var aKey = aKeys[nIdx];
+                return nIdx, aKey
+            }
 
 
         	// close opengov button
--- bookmarklet/opengov-button.js
+++ bookmarklet/opengov-button.js
@@ -1,16 +1,12 @@
 var openGovButtonCSS = document.createElement('link');
 openGovButtonCSS.rel = 'stylesheet';
 openGovButtonCSS.href = 'https://s3.amazonaws.com/sunlightlabs/opengov-button.css';
-// document.head.appendChild(openGovButtonCSS);
+document.head.appendChild(openGovButtonCSS);
 
 var jqueryInject = document.createElement('script');
 jqueryInject.src = 'https://code.jquery.com/jquery-1.8.3.min.js';
-// document.head.appendChild(jqueryInject);
+document.head.appendChild(jqueryInject);
 
 var openGovButtonInject = document.createElement('script');
 openGovButtonInject.src = 'https://s3.amazonaws.com/sunlightlabs/opengov-button-inject.js';
-// document.head.appendChild(openGovButtonInject);
-
-var openGovInjections = openGovButtonCSS + jqueryInject + openGovButtonInject;
-
-document.head.appendChild(openGovInjections)
+document.head.appendChild(openGovButtonInject);
