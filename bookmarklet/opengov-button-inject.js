971c6ab83f9a60bcc6780380577899a27aa24bf1
jalbertbowden <jalbertbowden@gmail.com>
2016-04-24 16:36:54 -0400

    Injection now has navigation and tabs for content; one is form; one if for tools; another is for info and the last is just here, aka blank atm

--- bookmarklet/opengov-button-inject.js
+++ bookmarklet/opengov-button-inject.js
@@ -16,12 +16,53 @@ function runScript() {
         		document.getElementsByTagName('head')[0].appendChild(jq);
         	}
 
-            var userDataList = "<ul class=\"xoxo\">\
+            var openGovButtonNav = "<div id=\"opengovbuttonnavbox\">\
+                <nav id=\"opengovbuttonnav\">\
+                  <ul class=\"ul-nav\">\
+                    <li><a class=\"data-link\" data-tab=\"tab-1\" href\"#form\">Form</a></li>\
+                    <li><a class=\"data-link\" data-tab=\"tab-2\" href\"#tools\">Tools</a></li>\
+                    <li><a class=\"data-link\" data-tab=\"tab-3\" href\"#info\">Info</a></li>\
+                    <li><a class=\"data-link\" data-tab=\"tab-4\" href\"#link\">Link 1</a></li>\
+                  </ul>\
+                </nav>\
+                </div>";
+
+            var openGovButtonNavForm = "<section id=\"tab-1\" class=\"opengovbutton-section\"><form id\"form-opengov-button\">\
+                <fieldset>\
+                  <legend>OpenGov Button Form</legend>\
+                  <label for=\"\"><b class=\"block\">Label</b>\
+                    <input type=\"\" id=\"\" placeholder=\"\" validate=\"\" />\
+                  </label>\
+                  <label for=\"\"><b class=\"block\">Label</b>\
+                    <input type=\"\" id=\"\" placeholder=\"\" validate=\"\" />\
+                  </label>\
+                  <label for=\"\"><b class=\"block\">Label</b>\
+                    <input type=\"\" id=\"\" placeholder=\"\" validate=\"\" />\
+                  </label>\
+                  <label for=\"\"><b class=\"block\">Label</b>\
+                    <input type=\"\" id=\"\" placeholder=\"\" validate=\"\" />\
+                  </label>\
+                </fieldset>\
+            </form></section>";
+
+            var openGovButtonTools = "<section id=\"tab-2\" class=\"opengovbutton-section\"><h2>Tools</h2></section>";
+
+            var userDataList = "<section id=\"tab-3\" class=\"opengovbutton-section\"><ul class=\"xoxo\">\
                 <li>Browser Code: <b id=\"user-data-browser\"></b></li>\
                 <li>Browser: <b id=\"user-browser\"></b></li>\
                 <li>Pathname: <b id=\"user-pathname\"></b></li>\
                 <li>URL: <b id=\"user-url\"></b></li>\
-                </ul>";
+                </ul></section>";
+
+            var openGovButtonSectionBlank = "<section id=\"tab-4\" class=\"opengovbutton-section\"><h2>Blank Section</h2></section>";
+
+
+            var oGBContent = openGovButtonNav + "<div id=\"opengovbutton-tabbed-content\">" + openGovButtonNavForm + openGovButtonTools + userDataList + openGovButtonSectionBlank + "</div>";
+
+
+            var openGovButtonBranding = "<a href\"https://sunlightfoundation.com\" id=\"opengovbutton-sf-logo\">Sunlight Foundation</a>";
+
+            var openGovButtonContentHeader = openGovButtonBranding + "<button id='closeopengovbutton' type='button'></button><div id='opengov-button-content'>";
 
 
             // var htmlSTring = "<div>\
@@ -29,7 +70,7 @@ function runScript() {
             //  </div>";
 
             // inject opengov button content
-            $("body").append("<div id='opengov-button'><button id='closeopengovbutton' type='button'></button><div id='opengov-button-content'>" + userDataList + "</div></div>");
+            $("body").append("<div id='opengov-button'>" + openGovButtonContentHeader + oGBContent + "</div></div>");
 
             var browserID, browserType = getBrowserId();
 
@@ -59,6 +100,22 @@ function runScript() {
         	$('#opengov-button').click(function() {
         		$('#opengov-button').hide();
         	});
+
+            // tabs
+            $('ul.tabs li').click(function(){
+                var tab_id = $(this).attr('data-tab');
+                $('ul.tabs li').removeClass('current');
+                $('.tab-content').removeClass('current');
+
+                $(this).addClass('current');
+                $("#"+tab_id).addClass('current');
+            })
+
+
+
+
+
+
         });
 
     } else {
