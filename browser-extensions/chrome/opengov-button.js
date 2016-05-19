ad30d57e218ffa181bddb8031bb6d3965efc4793
Albert Bowden <albert@sunlightfoundation.com>
2016-05-19 14:14:27 -0400

    button now reporting back correct window width

--- browser-extensions/chrome/opengov-button.js
+++ browser-extensions/chrome/opengov-button.js
@@ -22,6 +22,47 @@ document.addEventListener('DOMContentLoaded', function() {
   }, false);
 }, false);
 
+
+// access location object associated with current frame
+// in this case, we want to get the url of the current tab in the browser
+// set value of url into form control input attribute value for capturing the URL
+chrome.tabs.getSelected(null, function(tab) {
+  var tabUrl = tab.url;
+  var tabTitle = tab.title;
+  document.getElementById('url').value = tab.url;
+  document.getElementById('title').value = tab.title;
+  // sets placeholder value...not necessary as we are setting the value attribute
+  // document.getElementById('url').placeholder = "What";
+});
+
+
+
+// query the current window (anyone will do) and set the window size in the toolstrip
+function showDimensions() {
+    chrome.windows.getCurrent(function(w) {
+        var el = document.getElementById("windowsize");
+      el.innerHTML = w.width + " x " + w.height;
+      el.setAttribute("style", "width: " + w.width + "px; height: " + w.height + "px");
+    });
+}
+
+// Listen to the content script and when told there is a change, query again
+chrome.extension.onConnect.addListener(function(port, name) {    
+  console.assert(name == "resize");
+  port.onMessage.addListener(function() {
+      showDimensions();
+  });
+});
+
+window.onload = function() {
+  showDimensions();
+};
+
+
+
+
+
+
 // if not right, get the size of the tab
 var extensionWidth = $(window).width();
 console.log(extensionWidth);
